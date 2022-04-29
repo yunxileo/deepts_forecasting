12c8434f97f85a1c68e618c5f6b01308f56c5749
weip <weip@trip.com>
2022-04-29 10:24:30 +0800

    Deployed 6fbc560 with MkDocs version: 1.3.0

---
+++ gen_json.py
@@ -0,0 +1,19 @@
+import json
+
+import mkdocs_gen_files
+
+from griffe.docstrings.parsers import Parser
+from griffe.encoders import JSONEncoder
+from griffe.loader import GriffeLoader
+
+griffe = GriffeLoader().load_module("deepts_forecasting")
+serialized = json.dumps(
+    griffe,
+    cls=JSONEncoder,
+    indent=0,
+    full=True,
+    docstring_parser=Parser.google,
+    docstring_options={"ignore_init_summary": True},
+)
+with mkdocs_gen_files.open("griffe.json", "w") as fd:
+    fd.write(serialized)
